a28c014a12c592120b25f2949888d155f2bc933d
Dani Vakero <dramvaq@gmail.com>
2025-01-08 13:26:54 +0100

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Configuración de la página
+st.set_page_config(page_title="Hola neurona", page_icon="🧠")
+
+# Imagen cabecera
+st.image("./images/image.png", use_column_width=True)
+
+# Título de la página
+st.title("Hola neurona 🧠")
